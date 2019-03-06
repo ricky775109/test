4894c1c3e963055e30a5b96b3f6b56334e898b2c
ricky775109 <44311420+ricky775109@users.noreply.github.com>
2019-03-06 15:32:41 +0800

    12

--- route.js
+++ route.js
@@ -39,6 +39,40 @@ var appRouter = function (app) {
    }
 
  });
+
+ app.get("/student", function (req, res) {
+    var data = ({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      id: faker.random.uuid()
+    });
+    res.status(200).send(data);
+  });
+ 
+ app.get("/students/:num", function (req, res) {
+    var users = [];
+    var num = req.params.num;
+ 
+    if (isFinite(num) && num  > 0 ) {
+      for (i = 0; i <= num-1; i++) {
+        users.push({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            id: faker.random.uuid()
+         });
+      }
+ 
+      res.status(200).send(users);
+     
+    } else {
+      res.status(400).send({ message: 'invalid number supplied' });
+    }
+ 
+  });
 }
 
 module.exports = appRouter;
